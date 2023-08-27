1a3f6445fc074c2a22d4069ca519d6ca5e89064d
Jerry Hobby <Jerry@JerryHobby.com>
2023-08-27 15:20:50 -0500

    finished with optimizations for data and navigation

--- lib/story.dart
+++ lib/story.dart
@@ -1,6 +1,30 @@
+/*
+The Story class is a blueprint for a Story object.
+It has 3 properties which are all final because we don't want them to change after the Story has been created.
+*/
 
-//TODO: Step 2 - Create a new class called Story.
+class Story {
+  final String _storyTitle;
+  final String _choice1;
+  final String _choice2;
+  final int _next1;
+  final int _next2;
 
-//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+  Story({
+    required String storyTitle,
+    required String choice1,
+    required String choice2,
+    required int next1,
+    required int next2,
+  })  : _choice2 = choice2,
+        _choice1 = choice1,
+        _next1 = next1,
+        _next2 = next2,
+        _storyTitle = storyTitle;
 
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  get storyTitle => _storyTitle;
+  get choice1 => _choice1;
+  get choice2 => _choice2;
+  get next1 => _next1;
+  get next2 => _next2;
+}
